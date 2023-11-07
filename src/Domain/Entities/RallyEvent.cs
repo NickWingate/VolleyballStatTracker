fa0d5aed5309c5d9df333360ddcca15b886e254f
NickWingate <nicholas.wingate03@gmail.com>
2023-11-07 05:02:56 +0000

    Create RallyEvent Type

---
+++ src/Domain/Entities/RallyEvent.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Enums;
+
+namespace Domain.Entities;
+
+public class RallyEvent
+{
+    public EventType Type { get; set; }
+    public int PlayerResponsible { get; set; } //todo: can be player type eventually
+    public double Quality { get; set; }
+}
