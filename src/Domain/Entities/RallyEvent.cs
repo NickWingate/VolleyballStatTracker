b24a4c446b9a610629fe4c6da3c309468f39b98d
NickWingate <nicholas.wingate03@gmail.com>
2023-11-07 06:19:20 +0000

    Add string to event decoder

--- src/Domain/Entities/RallyEvent.cs
+++ src/Domain/Entities/RallyEvent.cs
@@ -6,5 +6,8 @@ public class RallyEvent
 {
     public EventType Type { get; set; }
     public int PlayerResponsible { get; set; } //todo: can be player type eventually
-    public double Quality { get; set; }
+    public double NumericQuality { get; set; }
+    public Position SetLocation { get; set; }
+    public AttackValue AttackResult { get; set; }
+    public BlockCredit BlockCredit { get; set; }
 }
